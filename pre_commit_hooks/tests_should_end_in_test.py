00d5904b803135f1179be3893ada0a69b0032e57
Anthony Sottile <asottile@umich.edu>
2014-06-01 16:08:31 -0700

    Fix pylint.

--- pre_commit_hooks/tests_should_end_in_test.py
+++ pre_commit_hooks/tests_should_end_in_test.py
@@ -10,9 +10,9 @@ def validate_files(argv):
     retcode = 0
     for filename in argv:
         if (
-            not filename.endswith('_test.py') and
-            not filename.endswith('__init__.py') and
-            not filename.endswith('/conftest.py')
+                not filename.endswith('_test.py') and
+                not filename.endswith('__init__.py') and
+                not filename.endswith('/conftest.py')
         ):
             retcode = 1
             print('{0} does not end in _test.py'.format(filename))
--- tests/trailing_whitespace_fixer_test.py
+++ tests/trailing_whitespace_fixer_test.py
@@ -6,8 +6,8 @@ from pre_commit_hooks.trailing_whitespace_fixer import fix_trailing_whitespace
 def test_fixes_trailing_whitespace(tmpdir):
     with local.cwd(tmpdir.strpath):
         for filename, contents in (
-            ('foo.py', 'foo \nbar \n'),
-            ('bar.py', 'bar\t\nbaz\t\n'),
+                ('foo.py', 'foo \nbar \n'),
+                ('bar.py', 'bar\t\nbaz\t\n'),
         ):
             with open(filename, 'w') as f:
                 f.write(contents)  # pragma: no cover (python 2.6 coverage bug)
@@ -16,8 +16,8 @@ def test_fixes_trailing_whitespace(tmpdir):
         assert ret == 1
 
         for filename, after_contents in (
-            ('foo.py', 'foo\nbar\n'),
-            ('bar.py', 'bar\nbaz\n'),
+                ('foo.py', 'foo\nbar\n'),
+                ('bar.py', 'bar\nbaz\n'),
         ):
             assert open(filename).read() == after_contents
 
