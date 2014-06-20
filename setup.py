c94b2087dba92f9111277ded435dea9473356cae
Anthony Sottile <asottile@umich.edu>
2014-06-19 17:51:42 -0700

    This is v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='pre_commit_hooks',
     description='Some out-of-the-box hooks for pre-commit.',
     url='https://github.com/pre-commit/pre-commit-hooks',
-    version='0.1.0',
+    version='0.1.1',
 
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
