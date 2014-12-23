e3312c773e9e3ac9b939bc3e0ca6a872dae5cdef
Anthony Sottile <asottile@umich.edu>
2014-12-23 12:04:19 -0800

    Use fileinput instead of sed.

--- pre_commit_hooks/trailing_whitespace_fixer.py
+++ pre_commit_hooks/trailing_whitespace_fixer.py
@@ -1,10 +1,16 @@
 from __future__ import print_function
 
 import argparse
+import fileinput
 import sys
 from plumbum import local
 
 from pre_commit_hooks.util import entry
+
+
+def _fix_file(filename):
+    for line in fileinput.input([filename], inplace=True):
+        print(line.rstrip())
 
 
 @entry
@@ -20,7 +26,7 @@ def fix_trailing_whitespace(argv):
     if bad_whitespace_files:
         for bad_whitespace_file in bad_whitespace_files:
             print('Fixing {0}'.format(bad_whitespace_file))
-            local['sed']['-i', '-e', 's/[[:space:]]*$//', bad_whitespace_file]()
+            _fix_file(bad_whitespace_file)
         return 1
     else:
         return 0
