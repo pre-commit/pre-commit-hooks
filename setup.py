97b88d9610bcc03982ddac33caba98bb2b751f5f
Anthony Sottile <asottile@umich.edu>
2016-02-11 09:46:10 -0800

    flake8 2.5.3 is broken: https://gitlab.com/pycqa/flake8/issues/119

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     packages=find_packages('.', exclude=('tests*', 'testing*')),
     install_requires=[
         # quickfix to prevent pep8 conflicts
-        'flake8',
+        'flake8!=2.5.3',
         'argparse',
         'autopep8>=1.1',
         'pyyaml',
