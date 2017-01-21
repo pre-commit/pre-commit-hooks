bec44063b092280763e12416f822258dca63c6be
Anthony Sottile <asottile@umich.edu>
2017-01-20 20:41:55 -0800

    Test py36 instead of py34

--- setup.py
+++ setup.py
@@ -14,11 +14,10 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
@@ -27,7 +26,6 @@ setup(
     install_requires=[
         # quickfix to prevent pep8 conflicts
         'flake8!=2.5.3',
-        'argparse',
         'autopep8>=1.1',
         'pyyaml',
         'simplejson',
