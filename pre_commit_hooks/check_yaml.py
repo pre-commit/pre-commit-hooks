2be0e8bfa8e1efa77961cfcbefb4423172d6f556
Bryan Culbertson <bryan.culbertson@gmail.com>
2017-02-22 22:09:35 -0800

    Pass tags to ignore to yaml parser (#1)

--- pre_commit_hooks/check_yaml.py
+++ pre_commit_hooks/check_yaml.py
@@ -13,8 +13,14 @@ except ImportError:  # pragma: no cover (no libyaml-dev / pypy)
 
 def check_yaml(argv=None):
     parser = argparse.ArgumentParser()
+    parser.add_argument('--ignore-tags', type=lambda s: s.split(','), default=[],
+                        help='Custom tags to ignore.')
     parser.add_argument('filenames', nargs='*', help='Yaml filenames to check.')
     args = parser.parse_args(argv)
+
+    # Ignore custom tags by returning None
+    for tag in args.ignore_tags:
+        Loader.add_constructor(tag, lambda *a, **k: None)
 
     retval = 0
     for filename in args.filenames:
