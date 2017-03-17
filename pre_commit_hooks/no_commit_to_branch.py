5abcaee46d6539e7a3313cd7b701cb8a24f12f20
Evan J. Felix <evan.felix@pnnl.gov>
2017-03-17 13:44:07 -0700

    remove extraneous modules

--- pre_commit_hooks/no_commit_to_branch.py
+++ pre_commit_hooks/no_commit_to_branch.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 
 import argparse
-import sys
 
 import util
 
--- tests/check_no_commit_to_branch_test.py
+++ tests/check_no_commit_to_branch_test.py
@@ -1,10 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import subprocess
-import sys
-import pytest
-
 from pre_commit_hooks.no_commit_to_branch import is_on_branch
 from pre_commit_hooks.no_commit_to_branch import main
 from pre_commit_hooks.util import cmd_output
@@ -13,11 +9,11 @@ from pre_commit_hooks.util import cmd_output
 def test_other_branch(temp_git_dir):
     with temp_git_dir.as_cwd():
         cmd_output('git', 'checkout', '-b', 'anotherbranch')
-        assert is_on_branch('master') == False
+        assert is_on_branch('master') is False
 
 def test_master_branch(temp_git_dir):
     with temp_git_dir.as_cwd():
-        assert is_on_branch('master') == True
+        assert is_on_branch('master') is True
 
 def test_main_other_call(temp_git_dir):
     with temp_git_dir.as_cwd():
