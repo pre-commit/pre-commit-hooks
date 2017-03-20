a8592669d958b27368e4621741c1f0cb5e538f36
Evan Felix <karcaw@gmail.com>
2017-03-20 10:36:51 -0700

    Add a no commit to specific branch hook. (#185)
    * add no commit code and config
    
    * add the code
    
    * remove version tweak
    
    * fix logic, remove newline
    
    * add Tests and cleanup testing issues
    
    * remove extraneous modules
    
    * cleanup some pep8 and flake issues
    
    * reorder imports
    
    * more fixes for syntax checking
    
    * code cleanup based off asottile comments
    
    * Use Contractions Properly, alphabatize new hook.
    
    * Adding support for branches with a slash in them.

---
+++ pre_commit_hooks/no_commit_to_branch.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+import argparse
+import sys
+
+from pre_commit_hooks.util import cmd_output
+
+
+def is_on_branch(protected):
+    branch = cmd_output('git', 'symbolic-ref', 'HEAD')
+    chunks = branch.strip().split('/')
+    return '/'.join(chunks[2:]) == protected
+
+
+def main(argv=[]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-b', '--branch', default='master', help='branch to disallow commits to')
+    args = parser.parse_args(argv)
+
+    return int(is_on_branch(args.branch))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
---
+++ tests/check_no_commit_to_branch_test.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from pre_commit_hooks.no_commit_to_branch import is_on_branch
+from pre_commit_hooks.no_commit_to_branch import main
+from pre_commit_hooks.util import cmd_output
+
+
+def test_other_branch(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        cmd_output('git', 'checkout', '-b', 'anotherbranch')
+        assert is_on_branch('master') is False
+
+
+def test_multi_branch(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        cmd_output('git', 'checkout', '-b', 'another/branch')
+        assert is_on_branch('master') is False
+
+
+def test_multi_branch_fail(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        cmd_output('git', 'checkout', '-b', 'another/branch')
+        assert is_on_branch('another/branch') is True
+
+
+def test_master_branch(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        assert is_on_branch('master') is True
+
+
+def test_main_b_call(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        cmd_output('git', 'checkout', '-b', 'other')
+        assert main(['-b', 'other']) == 1
+
+
+def test_main_branch_call(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        cmd_output('git', 'checkout', '-b', 'other')
+        assert main(['--branch', 'other']) == 1
+
+
+def test_main_default_call(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        cmd_output('git', 'checkout', '-b', 'anotherbranch')
+        assert main() == 0
