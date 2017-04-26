0e20aa09e25b06da652702b3bbb6260921bd0d4d
Oscar Landry <lasm.landry@gmail.com>
2017-04-26 10:04:26 +0200

    define more branch protected

--- pre_commit_hooks/no_commit_to_branch.py
+++ pre_commit_hooks/no_commit_to_branch.py
@@ -6,20 +6,26 @@ import sys
 from pre_commit_hooks.util import cmd_output
 
 
-def is_on_branch(protected):
+def is_on_branch(protected=()):
     branch = cmd_output('git', 'symbolic-ref', 'HEAD')
     chunks = branch.strip().split('/')
-    return '/'.join(chunks[2:]) == protected
+    position = '/'.join(chunks[2:])
+    return position in (protected or ('master',))
 
 
-def main(argv=[]):
+def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-b', '--branch', default='master', help='branch to disallow commits to')
+        '-b',
+        '--branch',
+        action='append',
+        dest='branches',
+        help='branch to disallow commits to'
+    )
     args = parser.parse_args(argv)
 
-    return int(is_on_branch(args.branch))
+    return int(is_on_branch(args.branches))
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv[1:]))
