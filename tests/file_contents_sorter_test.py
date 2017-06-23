05d9c8c8051446dc14aec45a859f39ae22c01bdb
Daniel Gallagher <daniellg@yelp.com>
2017-06-23 15:10:10 -0700

    Make tests pass

--- tests/file_contents_sorter_test.py
+++ tests/file_contents_sorter_test.py
@@ -2,7 +2,6 @@ import pytest
 
 from pre_commit_hooks.file_contents_sorter import FAIL
 from pre_commit_hooks.file_contents_sorter import main
-from pre_commit_hooks.file_contents_sorter import parse_commandline_input
 from pre_commit_hooks.file_contents_sorter import PASS
 
 
@@ -29,20 +28,3 @@ def test_integration(input_s, expected_retval, output, tmpdir):
 
     assert path.read_binary() == output
     assert output_retval == expected_retval
-
-
-def test_parse_commandline_input_errors_without_args():
-    with pytest.raises(SystemExit):
-        parse_commandline_input([])
-
-
-@pytest.mark.parametrize(
-    ('filename_list'),
-    (
-        ['filename1'],
-        ['filename1', 'filename2'],
-    )
-)
-def test_parse_commandline_input_success(filename_list):
-    args = parse_commandline_input(filename_list)
-    assert args.filenames == filename_list
