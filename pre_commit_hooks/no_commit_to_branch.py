93f319c1f8bcf89aca0e29e886ef16d4e7010e06
Anthony Sottile <asottile@umich.edu>
2018-02-19 12:56:14 -0800

    Fix no-commit-to-branch when not on a branch

--- pre_commit_hooks/no_commit_to_branch.py
+++ pre_commit_hooks/no_commit_to_branch.py
@@ -1,21 +1,25 @@
 from __future__ import print_function
 
 import argparse
-import sys
 
+from pre_commit_hooks.util import CalledProcessError
 from pre_commit_hooks.util import cmd_output
 
 
 def is_on_branch(protected):
-    branch = cmd_output('git', 'symbolic-ref', 'HEAD')
+    try:
+        branch = cmd_output('git', 'symbolic-ref', 'HEAD')
+    except CalledProcessError:
+        return False
     chunks = branch.strip().split('/')
     return '/'.join(chunks[2:]) == protected
 
 
-def main(argv=[]):
+def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-b', '--branch', default='master', help='branch to disallow commits to',
+        '-b', '--branch', default='master',
+        help='branch to disallow commits to',
     )
     args = parser.parse_args(argv)
 
@@ -23,4 +27,4 @@ def main(argv=[]):
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    exit(main())
