b9cc9d7761af788363515a49bf2a47f22c309975
Anthony Sottile <asottile@umich.edu>
2020-05-14 16:00:29 -0700

    remove flake8 and suggest pycqa/flake8

---
+++ pre_commit_hooks/removed.py
@@ -0,0 +1,15 @@
+import sys
+from typing import Optional
+from typing import Sequence
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    argv = argv if argv is not None else sys.argv[1:]
+    hookid, new_hookid, url = argv
+    raise SystemExit(
+        f'`{hookid}` has been removed -- use `{new_hookid}` from {url}',
+    )
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ tests/removed_test.py
@@ -0,0 +1,16 @@
+import pytest
+
+from pre_commit_hooks.removed import main
+
+
+def test_always_fails():
+    with pytest.raises(SystemExit) as excinfo:
+        main((
+            'autopep8-wrapper', 'autopep8',
+            'https://github.com/pre-commit/mirrors-autopep8',
+        ))
+    msg, = excinfo.value.args
+    assert msg == (
+        '`autopep8-wrapper` has been removed -- '
+        'use `autopep8` from https://github.com/pre-commit/mirrors-autopep8'
+    )
