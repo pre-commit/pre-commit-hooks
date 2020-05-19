926208fb31c7f4b6e718e20fc1f5789fd74dc3df
Pedro Calleja <pedro@urbvan.com>
2020-05-18 21:27:24 -0500

    hotfix: the user may be define an extra arguments for a removed hook

--- pre_commit_hooks/removed.py
+++ pre_commit_hooks/removed.py
@@ -5,7 +5,7 @@ from typing import Sequence
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     argv = argv if argv is not None else sys.argv[1:]
-    hookid, new_hookid, url = argv
+    hookid, new_hookid, url = argv[:3]
     raise SystemExit(
         f'`{hookid}` has been removed -- use `{new_hookid}` from {url}',
     )
