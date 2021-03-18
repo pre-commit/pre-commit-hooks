f681234a0e457afc6c4f2774d646dd6454d655a0
Krystian Chmura <krystian.chmura@gmail.com>
2021-03-19 00:59:31 +0100

    check also directories in check_case_conflict
    fix #70

--- tests/check_case_conflict_test.py
+++ tests/check_case_conflict_test.py
@@ -1,6 +1,23 @@
+import sys
+
+import pytest
+
 from pre_commit_hooks.check_case_conflict import find_conflicting_filenames
 from pre_commit_hooks.check_case_conflict import main
+from pre_commit_hooks.check_case_conflict import parents
 from pre_commit_hooks.util import cmd_output
+
+skip_win32 = pytest.mark.skipif(
+    sys.platform == 'win32',
+    reason='case conflicts between directories and files',
+)
+
+
+def test_parents():
+    assert set(parents('a')) == set()
+    assert set(parents('a/b')) == {'a'}
+    assert set(parents('a/b/c')) == {'a/b', 'a'}
+    assert set(parents('a/b/c/d')) == {'a/b/c', 'a/b', 'a'}
 
 
 def test_nothing_added(temp_git_dir):
@@ -26,6 +43,36 @@ def test_adding_something_with_conflict(temp_git_dir):
         assert find_conflicting_filenames(['f.py', 'F.py']) == 1
 
 
+@skip_win32  # pragma: win32 no cover
+def test_adding_files_with_conflicting_directories(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        temp_git_dir.mkdir('dir').join('x').write('foo')
+        temp_git_dir.mkdir('DIR').join('y').write('foo')
+        cmd_output('git', 'add', '-A')
+
+        assert find_conflicting_filenames([]) == 1
+
+
+@skip_win32  # pragma: win32 no cover
+def test_adding_files_with_conflicting_deep_directories(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        temp_git_dir.mkdir('x').mkdir('y').join('z').write('foo')
+        temp_git_dir.join('X').write('foo')
+        cmd_output('git', 'add', '-A')
+
+        assert find_conflicting_filenames([]) == 1
+
+
+@skip_win32  # pragma: win32 no cover
+def test_adding_file_with_conflicting_directory(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        temp_git_dir.mkdir('dir').join('x').write('foo')
+        temp_git_dir.join('DIR').write('foo')
+        cmd_output('git', 'add', '-A')
+
+        assert find_conflicting_filenames([]) == 1
+
+
 def test_added_file_not_in_pre_commits_list(temp_git_dir):
     with temp_git_dir.as_cwd():
         temp_git_dir.join('f.py').write("print('hello world')")
@@ -44,6 +91,19 @@ def test_file_conflicts_with_committed_file(temp_git_dir):
         cmd_output('git', 'add', 'F.py')
 
         assert find_conflicting_filenames(['F.py']) == 1
+
+
+@skip_win32  # pragma: win32 no cover
+def test_file_conflicts_with_committed_dir(temp_git_dir):
+    with temp_git_dir.as_cwd():
+        temp_git_dir.mkdir('dir').join('x').write('foo')
+        cmd_output('git', 'add', '-A')
+        cmd_output('git', 'commit', '--no-gpg-sign', '-n', '-m', 'Add f.py')
+
+        temp_git_dir.join('DIR').write('foo')
+        cmd_output('git', 'add', '-A')
+
+        assert find_conflicting_filenames([]) == 1
 
 
 def test_integration(temp_git_dir):
