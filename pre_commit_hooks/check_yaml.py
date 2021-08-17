3bdd0b0802213574bbbaf7fc839373e16a2aebc0
seroy <seroy@bk.ru>
2021-08-17 23:10:10 +0300

    Fix "while scanning a plain scalar found unexpected ':'"

--- pre_commit_hooks/check_yaml.py
+++ pre_commit_hooks/check_yaml.py
@@ -7,7 +7,7 @@ from typing import Sequence
 
 import ruamel.yaml
 
-yaml = ruamel.yaml.YAML(typ='safe')
+yaml = ruamel.yaml.YAML(typ='safe', pure=True)
 
 
 def _exhaust(gen: Generator[str, None, None]) -> None:
