ccdf02dfd48be0656f3b33ded45e629296344db5
Luís Ferreira <contact@lsferreira.net>
2021-10-02 20:33:35 +0100

    detect_private_key: add textual version of `PKCS #8` encrypted private keys
    As described by RFC7468 and RFC5958, keys that are encoded using the "ENCRYPTED
    PRIVATE KEY" label are described as private key information and therefore can
    contain secrets, even though encrypted.
    
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- tests/detect_private_key_test.py
+++ tests/detect_private_key_test.py
@@ -10,6 +10,7 @@ TESTS = (
     (b'-----BEGIN OPENSSH PRIVATE KEY-----', 1),
     (b'PuTTY-User-Key-File-2: ssh-rsa', 1),
     (b'---- BEGIN SSH2 ENCRYPTED PRIVATE KEY ----', 1),
+    (b'-----BEGIN ENCRYPTED PRIVATE KEY-----', 1),
     (b'ssh-rsa DATA', 0),
     (b'ssh-dsa DATA', 0),
     # Some arbitrary binary data
