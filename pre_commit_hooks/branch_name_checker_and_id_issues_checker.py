81f576ddcb1ebaae8d21fd454e6952bc5da8d4ec
Cristinescu Liviu <liviu.cristinescu@elektrobit.com>
2023-11-07 11:20:39 +0000

    Try new hooks

---
+++ pre_commit_hooks/branch_name_checker_and_id_issues_checker.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import argparse
+import re
+from typing import AbstractSet
+from typing import Sequence
+from pre_commit_hooks.util import CalledProcessError
+from pre_commit_hooks.util import cmd_output
+
+
+def is_on_branch(
+        protected: AbstractSet[str],
+        patterns: AbstractSet[str] = frozenset(),
+) -> bool:
+    try:
+        ref_name = cmd_output('git', 'symbolic-ref', 'HEAD')
+    except CalledProcessError:
+        return False
+    chunks = ref_name.strip().split('/')
+    branch_name = '/'.join(chunks[2:])
+    return branch_name in protected or any(
+        re.match(p, branch_name) for p in patterns
+    )
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-b', '--branch', action='append',
+        help='branch to disallow commits to, may be specified multiple times',
+    )
+    parser.add_argument(
+        '-p', '--pattern', action='append',
+        help=(
+            'regex pattern for branch name to disallow commits to, '
+            'may be specified multiple times'
+        ),
+    )
+    args = parser.parse_args(argv)
+
+    protected = frozenset(args.branch or ('master', 'main'))
+    patterns = frozenset(args.pattern or ())
+    result = int(is_on_branch(protected, patterns))
+    print(result)
+    return result
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
